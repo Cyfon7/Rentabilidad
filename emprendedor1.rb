064408250f89a88d1416092021868fed49206b29
Jorge <jorgesifontes.cl@gmail.com>
2020-10-13 21:51:25 -0300

    end emprendedor 1 file

--- emprendedor1.rb
+++ emprendedor1.rb
@@ -1,11 +1,16 @@
 precio_venta = ARGV[0].to_f
 usuarios = ARGV[1].to_i
 gastos = ARGV[2].to_f
-Impuesto = 35
 
 utilidades_b = (precio_venta * usuarios) - gastos
 
-utilidades_n = utilidades_b * ((100 - Impuesto)/100)
+if utilidades_b > 0
+    impuesto = 0.35
+else
+    impuesto = 0
+end
 
-puts "Utilidades bruta =  #{utilidades_b}"
-puts "Utilidades Neta = #{utilidades_n}"
+utilidades_n = utilidades_b * (1 - impuesto)
+
+puts "Utilidad Bruta = #{utilidades_b}"
+puts "Utilidad Neta = #{utilidades_n}"
